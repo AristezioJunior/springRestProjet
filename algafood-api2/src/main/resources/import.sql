4eab2a30cd3e5031909d151c5bc772f6917cada2
AristezioJunior <57036236+AristezioJunior@users.noreply.github.com>
2021-02-25 22:41:05 -0300

    3.18
    3.18

--- algafood-api2/src/main/resources/import.sql
+++ algafood-api2/src/main/resources/import.sql
@@ -1,8 +1,10 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
-insert into restaurante (nome, taxa_frete) values ('Dona Florinda', 5);
-insert into restaurante (nome, taxa_frete) values ('Aristezio Bar', 3);
-insert into restaurante (nome, taxa_frete) values ('Malakas Bar', 3.50);
-insert into restaurante (nome, taxa_frete) values ('Thai Gourmet', 10.00);
-insert into restaurante (nome, taxa_frete) values ('Thai Delivery', 9.50);
-insert into restaurante (nome, taxa_frete) values ('Tuk Tuk Comida Indiana', 15.00);
+insert into cozinha (id, nome) values (1,'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
+
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Dona Florinda', 5, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Aristezio Bar', 3, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Malakas Bar', 3.50, 1);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10.00, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 2);
+insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15.00, 2);
